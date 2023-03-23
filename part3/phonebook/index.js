6c7b9ba5d6c007abea19785c034a023ff913a5ba
Dheeraj <i.dheerajdileep@gmail.com>
2023-03-23 22:59:57 +0530

    completed 3.7,3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,18 @@
 const http = require('http')
 const express = require('express')
 const app = express()
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 app.use(express.json())
+app.use(requestLogger)
 let persons = 
 [
     { 
@@ -77,7 +88,7 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
   response.status(204).end()
 })
-
+app.use(unknownEndpoint)
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
