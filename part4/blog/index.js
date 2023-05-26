cb6bbd1f98301952a682d8501b92eee44dd89104
Dheeraj <i.dheerajdileep@gmail.com>
2023-05-26 20:55:42 +0530

    4.2 refactoring complete

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -1,59 +1,8 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
+const app = require('./app')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose
-  .connect(config.mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    logger.info("Connected to mongodb")
-  })
-  .catch((error) => {
-    logger.error("Error connecting to MongoDB:", error.message);
-  });
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs);
-    })
-    .catch(error => {
-      logger.error("Error retrieving blogs:", error.message);
-      response.status(500).json({ error: "An error occurred" });
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result);
-    })
-    .catch(error => {
-      logger.error("Error saving blog:", error.message);
-      response.status(500).json({ error: "An error occurred" });
-    });
-});
-
 const PORT  = config.PORT 
-
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`);
 });
