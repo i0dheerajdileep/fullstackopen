21be33b60ff52c8e69a450fe902c049213e8735c
Dheeraj <i.dheerajdileep@gmail.com>
2023-05-29 16:53:00 +0530

    functionalities done

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require('./controllers/login')
 const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
@@ -24,5 +26,9 @@ app.use(middleware.requestLogger)
 app.use("/api/blogs", blogsRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(middleware.tokenExtractor);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
 
 module.exports = app
